75861ebcb40f85ca4538a92127f5b96465a3e22f
suhailgme <github@suhailg.me>
2017-07-13 03:36:00 -0600

    -Change name of getMarketDepth to getMarketRate to better reflect functionality. - add coin parameter to activeLoans to allow a search for a specific currency you have loaned. -Add getLendingEarnings function to calculate gross and net loan earnings.

--- worker.js
+++ worker.js
@@ -1,8 +1,11 @@
 const loans = require('./loansWorking')
 const _ = require('lodash')
 
+var coin = 'BTC'
+var requiredDepth = 33
 
-let getMarketDepth = coin => {
+
+const getMarketRate = coin => {
     var marketDepth = loans.getDepth(coin).then((ticker) => {
         let depth = 0,
             count = 0,
@@ -19,12 +22,16 @@ let getMarketDepth = coin => {
 }
 
 
-
-const activeLoans = () => {
+const activeLoans = (coin) => {
     loans.getActiveLoans()
         .then((activeLoans) => {
+            console.log(activeLoans)
             if (activeLoans.provided.length === 0) {
                 console.log('No active loans')
+            } else {
+                activeLoans.provided.forEach((loan) => {
+                    console.log(`Loan of ${loan.amount}${coin} at rate of ${(loan.rate*100).toFixed(4)}% for ${loan.duration} days accepted on ${loan.date} (auto renew is ${loan.autoRenew === 0 ? 'off).' : 'on).'}`)
+                })
             }
         }).catch(err => console.error(err.message))
 }
@@ -35,8 +42,34 @@ const getBalances = () => {
         .catch(err => console.error(err))
 }
 
+const getLendingEarnings = (coin) => {
+    loans.getLendingHistory().then((history) => {
+        if (history.length === 0) {
+            console.log("No lending history available.")
+        } else {
+            let gross = 0,
+                net = 0,
+                fees = 0
+            history.forEach((loan) => {
+                if (loan.currency === coin) {
+                    gross += parseFloat(loan.earned)
+                    fees += parseFloat(loan.fee)
+                }
+            })
+            console.log(`Gross earnings: ${gross} ${coin}\nFees: ${fees} ${coin}\nNet earnings: ${gross+fees} ${coin}`)
+        }
+    }).catch(err => console.error(err))
+}
 
-var coin = 'BTC'
-var requiredDepth = 33
-getMarketDepth(coin)
-activeLoans()
+// const openLoanOffers = () => {
+//     loans.getOpenLoanOffers().then(openOffers => console.log(openOffers))
+// }
+
+
+// getMarketRate(coin)
+activeLoans(coin)
+
+
+//getMarketDepth(coin)
+//getBalances()
+//getLendingEarnings(coin)
