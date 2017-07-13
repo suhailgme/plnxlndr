aabcaba55d91a26466297078d5717d4a8c60c93d
suhailgme <github@suhailg.me>
2017-07-13 03:34:08 -0600

    Refactor loans.js - Remove promises (poloniex-api-node returns promises already).

--- loans.js
+++ loans.js
@@ -1,66 +1,54 @@
 const Poloniex = require('poloniex-api-node');
 const _ = require('lodash')
 
-
-
 const apiKey = 'YOUR-32-CHARACTER-API-KEY'
 const secret = 'YOUR 128 CHARACTER ALPHANUMERIC SECRET KEY'
 
 let poloniex = new Poloniex(apiKey, secret);
 
+/*------------------------------------------------*/
 
-let getDepth = coin => {
-  return new Promise((resolve, reject) => {
-    return poloniex.returnLoanOrders(coin, null)
-    .then(loanOrders => resolve(loanOrders))
-    .catch(err => reject(err))
-  })
+const getDepth = coin => {
+  return poloniex.returnLoanOrders(coin, null)
+    .then(loanOrders => loanOrders)
+    .catch(err => err)
+}
+
+
+// Get lending history from begining of the year to present (2017 - hardcoded for now)
+const getLendingHistory = () => {
+  return poloniex.returnLendingHistory(1483228800, 1514764799, null)
+    .then(history => history)
+    .catch(err => history)
+}
+
+
+const getBalances = () => {
+  return poloniex.returnBalances()
+    .then(balances => balances)
+    .catch(err => err)
 }
 
 
 
-
-let getLendingHistory = () => {
-  poloniex.returnLendingHistory(0, 0, null).then(history => console.log(history))
-    .catch(err => console.error(err.message))
-}
-
-
-let getBalances = () => {
-  return new Promise((resolve, reject) => {
-    return poloniex.returnBalances()
-      .then(balances => resolve(balances))
-      .catch(err => reject(err))
-  })
-}
-
-
-//
-let getActiveLoans = () => {
-  return new Promise((resolve, reject) => {
+const getActiveLoans = () => {
     return poloniex.returnActiveLoans()
-      .then(activeLoans => resolve(activeLoans))
-      .catch(err => reject(err))
-  })
+      .then(activeLoans => activeLoans)
+      .catch(err => err)
+
 }
 
 
-let getOpenLoanOffers = (coin) => {
-  poloniex.returnOpenLoanOffers().then((loanOffers) => {
-    //console.log(loanOffers[coin].length)
-    if (loanOffers[coin].length > 0) {
-      loanOffers[coin].forEach((offer) => {
-        console.log(`Open offer of ${offer.amount}${coin} at rate of ${(offer.rate*100).toFixed(4)}% for ${offer.duration} days created on ${offer.date} (auto renew is ${offer.autoRenew === 0 ? 'off).' : 'on).'}`)
-      })
-    } else {
-      console.log('No open orders.')
-    }
-  }).catch(err => console.error(err.msg))
-
+const getOpenLoanOffers = coin => {
+  return poloniex.returnOpenLoanOffers()
+    .then(loanOffers => loanOffers)
+    .catch(err => err)
 }
 
 module.exports = {
   getActiveLoans,
   getDepth,
-  getBalances
+  getBalances,
+  getLendingHistory,
+  getOpenLoanOffers
 }
