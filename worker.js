7affe40db2ce4d57526b228826c38eccdb4ab86d
suhailgme <github@suhailg.me>
2017-07-18 04:38:04 -0600

    Update getMarketRate - getMarketRate returns a promise containing the current loan rate at the specified market depth. - Add comments to functions - Add main function to handle periodic calls to API.

--- worker.js
+++ worker.js
@@ -5,27 +5,43 @@ var coin = 'BTC'
 var requiredDepth = 33
 
 
-const getMarketRate = coin => {
-    var marketDepth = loans.getDepth(coin).then((ticker) => {
-        let depth = 0,
-            count = 0,
-            rate
-        ticker.offers.forEach((offer) => {
-            depth += Number(offer.amount)
-            count++
-            rate = offer.rate
-        })
-        console.log(`Required Market Depth is: ${requiredDepth}`)
-        console.log(`Market depth is ${depth} ${coin}. Total offers: ${count}. Rate is: ${_.round(rate,6) * 100}%`);
-        return ticker
-    }).catch(err => console.error(err.message))
+let loanMarketData = {
+    rate: [],
+    average: 0
+}
+
+// Takes the coin and required market depth (default 33) to get loan offers for. 
+// Prints the market depth, loan rate and number of offers parsed at the requested depth and
+// returns a promise with the market rate.
+const getMarketRate = (coin, requiredDepth, ) => {
+    return new Promise((resolve, reject) => {
+        var marketDepth = loans.getDepth(coin).then((ticker) => {
+            let depth = 0,
+                count = 0,
+                rate
+            ticker.offers.forEach((offer) => {
+                count++
+                if (depth <= requiredDepth) {
+                    //console.log(`Depth: ${depth} BTC, Offer Amount: ${offer.amount} BTC, Rate: ${offer.rate}%`)
+                    depth += Number(offer.amount)
+                    rate = parseFloat(offer.rate*100)
+                }
+            })
+            //console.log(`Required Market Depth is: ${requiredDepth}`)
+            console.log(`Market depth is ${depth} ${coin}. Total offers parsed: ${count}. Rate is: ${rate}%`);
+            depth ? resolve(rate):reject('Couldn\'t reach Poloniex')
+        }).catch(err => console.error(err.message))
+    })
+
 }
 
 
+// Takes the coin to check active loans for as argument and prints all current, ongoing loans including
+// amount loaned,rate, date accepted and whether auto renew is on or off.
 const activeLoans = (coin) => {
     loans.getActiveLoans()
         .then((activeLoans) => {
-            console.log(activeLoans)
+            //console.log(activeLoans)
             if (activeLoans.provided.length === 0) {
                 console.log('No active loans')
             } else {
@@ -36,12 +52,15 @@ const activeLoans = (coin) => {
         }).catch(err => console.error(err.message))
 }
 
-const getBalances = () => {
-    loans.getBalances()
+// Prints complete account balance for all accounts (exchange, margin, lending) to console.
+const getCompleteBalances = () => {
+    loans.getAllBalances()
         .then(balances => console.log(balances))
         .catch(err => console.error(err))
 }
 
+// Takes the coin to get earnings for as an argument. Prints the Gross earnings, fees and net earnings (gross - fees)
+// to console.
 const getLendingEarnings = (coin) => {
     loans.getLendingHistory().then((history) => {
         if (history.length === 0) {
@@ -66,10 +85,22 @@ const getLendingEarnings = (coin) => {
 // }
 
 
-// getMarketRate(coin)
-activeLoans(coin)
+const main = () => {
+    getMarketRate(coin, requiredDepth).then(rate =>loanMarketData.rate.push(rate))
+    console.log(loanMarketData)
+    loanMarketData.average = (loanMarketData.rate.length >= 1 ? _.mean(loanMarketData.rate) : console.log('Getting additional lending rates...'))
+    setTimeout(main, 10000)
+
+
+}
+
+// getMarketRate(coin, requiredDepth)
+// setInterval(getMarketRate, 10000, coin, requiredDepth)
+// getLendingEarnings(coin)
+// activeLoans(coin)
+main()
 
 
 //getMarketDepth(coin)
-//getBalances()
+//getCompleteBalances()
 //getLendingEarnings(coin)
